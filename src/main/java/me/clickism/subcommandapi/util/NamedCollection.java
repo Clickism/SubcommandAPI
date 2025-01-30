0e3f2daac5a82f0609a1fd793b558e68d07e8dfb
Clickism <yagizaktasx@gmail.com>
2025-01-30 16:37:35 +0100

    rename factory methods for clarity

--- src/main/java/me/clickism/subcommandapi/util/NamedCollection.java
+++ src/main/java/me/clickism/subcommandapi/util/NamedCollection.java
@@ -121,8 +121,8 @@ public class NamedCollection<T extends Named> extends AbstractCollection<T> {
      * @param strings collection of strings
      * @return named collection
      */
-    public static NamedCollection<Named> of(Collection<String> strings) {
-        return NamedCollection.of(strings, ArrayList::new);
+    public static NamedCollection<Named> ofStrings(Collection<String> strings) {
+        return NamedCollection.ofStrings(strings, ArrayList::new);
     }
 
     /**
@@ -132,7 +132,7 @@ public class NamedCollection<T extends Named> extends AbstractCollection<T> {
      * @param collectionSupplier collection supplier
      * @return named collection
      */
-    public static NamedCollection<Named> of(Collection<String> strings, Supplier<Collection<Named>> collectionSupplier) {
+    public static NamedCollection<Named> ofStrings(Collection<String> strings, Supplier<Collection<Named>> collectionSupplier) {
         return new NamedCollection<>(strings.stream()
                 .map(name -> (Named) () -> name)
                 .collect(Collectors.toCollection(collectionSupplier)));
